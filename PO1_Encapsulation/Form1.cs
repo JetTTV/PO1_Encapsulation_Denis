bf2b288df38361a997110f886044def7fe4af623
Jet <157501165+JetTTV@users.noreply.github.com>
2024-02-08 14:15:31 -0500

    Pvs et attaque

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,16 +2,27 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new Personnage();
+        Personnage dragon = new Personnage();
         public Form1()
         {
             InitializeComponent();
-            Personnage joueur = new Personnage();
             joueur.Pv = 10;
+            dragon.Pv = joueur.Pv * 3;
+            joueur.Strength = 1;
+            dragon.Strength = joueur.Strength * 3;
+
             MessageBox.Show(joueur.Pv.ToString());
-            DateTime rightNow = DateTime.Now;
-            rightNow
+            
         }
 
+        private void ActualiserLabel()
+        {
+            lbl_pointsVieJoueur.Text = "Health Points: " + joueur.Pv.ToString();
+            lbl_pointsVieDragon.Text = "Health Points: " + dragon.Pv.ToString();
+            lbl_puissanceAttaqueJoueur.Text ="Attack Power: " + joueur.Strength.ToString();
+            lbl_puissanceAttaqueDragon.Text = "Attack Power: " + dragon.Strength.ToString();
+        }
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
 
